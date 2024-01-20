e5d29b50e26453af948c908a569064834acd14a2
kairshma yadav <Karishmasvryadav@gmail.com>
2024-01-20 08:36:00 +0000

    ji

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const AccountName ="Saving";
+let AccountBalance= 2564;
+var Withdrawal = 200;
+RemainingBalance = 500;
+console.log(RemainingBalance);
+console.table([AccountBalance,AccountName,Withdrawal,RemainingBalance]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict" // treat all Js code as a newar version;
+
+
+let namea = "karishma";
+let age = 21;
+let isloggedin = true;
+//symbol = for uniqness component
+// alert(3+3);  // throw ans error we are using Nodejs not Browser  
+console.log(typeof null);  //Object
+console.log(typeof undefined); //undefined
+console.log(typeof namea); // string
+console.log(typeof age); // number
+console.log(typeof isloggedin); //boolean
