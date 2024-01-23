c456165e536ca7d66ba60232f9a7a19b2071045e
kairshma yadav <Karishmasvryadav@gmail.com>
2024-01-23 06:41:28 +0000

    date

---
+++ 01_basic/08_dates.js
@@ -0,0 +1,13 @@
+const myDate = new Date();
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleTimeString());
+
+
+console.log(typeof myDate);
