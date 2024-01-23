48c1d523e71c55af15e4b35a73bf705acfbe07a3
kairshma yadav <Karishmasvryadav@gmail.com>
2024-01-23 08:06:37 +0000

    date

--- 01_basic/08_dates.js
+++ 01_basic/08_dates.js
@@ -1,13 +1,26 @@
 const myDate = new Date();
-console.log(myDate);
+// console.log(myDate);
 
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toISOString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+
+
+// console.log(typeof myDate);
+
+const dateNow =Date.now();
+console.log(dateNow);
+
+console.log(`my Date is ${myDate}`)
+
 console.log(myDate.toLocaleDateString());
+console.log(myDate.getFullYear());
 
-console.log(myDate.toJSON());
-console.log(myDate.toLocaleTimeString());
-
-
-console.log(typeof myDate);
+setTimeout(()=>{
+    let date = Date.now();
+    console.log(`time difference is ${(Math.floor(date-dateNow))/1000}`)
+},3000)
