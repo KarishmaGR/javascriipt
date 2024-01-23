b44d3bd890f38e9cadb3177d246ec31d125c1df3
kairshma yadav <Karishmasvryadav@gmail.com>
2024-01-23 06:05:11 +0000

    math and Number

---
+++ 01_basic/07_nums_and_math.js
@@ -0,0 +1,31 @@
+const num = 100;
+const num1 = new Number(200);
+
+// console.log(num.toString().length);
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(4));
+// const e = Number.EPSILON
+// console.log(10*e);
+// console.log(Number.EPSILON^10);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// // console.log(EPSILON^4);
+
+// MAth Functions in javascript
+
+const num3 = 454.6454
+
+console.log(Math.round(num3));
+console.log(Math.ceil(num3));
+console.log(Math.floor(num3));
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10));
+// console.log((Math.random()*10)+1);
+
+const min = 10;
+const max =50;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
